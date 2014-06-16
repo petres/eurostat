9504d7009269a932774a9f481681024033c4e138
unknown <preschen@OP050.vista.wsr.ac.at>
2014-06-16 13:41:34 +0200

    bat files also with argument support

--- app/run.bat
+++ app/run.bat
@@ -2,5 +2,5 @@
 set OLDDIR=%CD%
 cd %~p0
 call utils\config.bat
-call %%python%% src\main.py
+call %%python%% src\main.py %*
 cd %OLDDIR%
--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 set OLDDIR=%CD%
 cd %~p0
 call app\utils\config.bat
-call %%python%% app\src\main.py
+call %%python%% app\src\main.py %*
 cd %OLDDIR%
